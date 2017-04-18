6f4c49e7bcc972cd210b3795515c12296ec38dcb
Jacob Rickerd <rickerd@umich.edu>
2017-04-18 13:40:30 -0400

    Updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'rtapi',
   packages = ['rtapi'],
-  version = '0.1',
+  version = '0.2',
   description = 'Wrapper for RT API',
   author = 'Jacob Rickerd',
   author_email = 'jacobrickerd@gmail.com',
+  license='MIT',
   url = 'https://github.com/Rickerd0613/rtapi',
-  download_url = 'https://github.com/peterldowns/rtapi/archive/0.1.tar.gz',
+  download_url = 'https://github.com/peterldowns/rtapi/archive/0.2.tar.gz',
   keywords = ['request', 'tracker', 'rt', 'api'],
-  classifiers = [],
+  install_requires=[
+	'requests',
+  ],
 )
